0cacb7c38ca0b724e8d49b22292a46c3ad307a1f
Tiril Gunnuldsen <32484555+tiriltoppen@users.noreply.github.com>
2021-03-26 23:02:24 +0100

    filene som er blir brukt
    dette er i tilfelle github desktop ikke funker. her legger inn alle filene jeg jeg har brukt og laget

---
+++ Home.js
@@ -0,0 +1,10 @@
+import Movies from '../components/Movies';
+
+const Home = () => (
+    <section>
+<h2>hey</h2>
+    <Movies />
+</section>
+);
+
+export default Home;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-oblig4
+Her lager du alle komponentene dine. Eks Movies, Movie. De igjen kan du bruke inne på sidene (pages)
